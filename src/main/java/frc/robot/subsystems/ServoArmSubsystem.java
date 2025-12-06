c5a17efd4eaaa4dd00d5198b34b2facd860f8ab6
GavinWu42711 <gavinwu42711@gmail.com>
2025-12-06 13:53:01 -0500

    Big arm
    Added subsystem for arm made by Monique. Servos missing proper number (don't have the arm right now)

--- src/main/java/frc/robot/subsystems/ServoArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ServoArmSubsystem.java
@@ -1,7 +1,34 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Servo;
 
 public class ServoArmSubsystem extends SubsystemBase{
-     
+    // PWM's servo's are connecte to
+     private final Servo m_servo1 = new Servo(1);
+     private final Servo m_servo2 = new Servo(2);
+     private final Servo m_bottomServo = new Servo(3);
+     private final Servo m_leftClawServo = new Servo(4);
+     private final Servo m_rightClawServo = new Servo(5);
+
+    public ServoArmSubsystem(){
+
+    }
+
+    // Rotates the longest section of the arm connected to the bottom servo
+    public void rotateArmTo(int degree){
+        m_bottomServo.setAngle(degree);
+    }
+
+    // Brings servos to eachother to grab something
+    public void closeClaw(){
+        m_leftClawServo.setAngle(90);
+        m_rightClawServo.setAngle(90);
+    }
+
+    // Brings servos apart to release something
+    public void openClaw(){
+        m_leftClawServo.setAngle(0);
+        m_rightClawServo.setAngle(0);
+    }
 }
