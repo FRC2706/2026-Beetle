6c015d46da19ec5fb282693aa70855df42c2d64c
GavinWu42711 <gavinwu42711@gmail.com>
2025-12-06 10:37:02 -0500

    Update ServoSubsystem.java

--- src/main/java/frc/robot/subsystems/ServoSubsystem.java
+++ src/main/java/frc/robot/subsystems/ServoSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Servo;
 
 public class ServoSubsystem extends SubsystemBase{
+    // Servo is connected to PWM 0
     private final Servo m_Servo = new Servo(0);
 
     public void turnTo(int degree){
